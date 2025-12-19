1a6072a7129fa0ee1ea51f9cbca1056bfdf30285
savio-sandesh <sandesh.cse00@gmail.com>
2025-12-19 11:23:11 +0530

    19_dec_2025

---
+++ core-csharp-practice/gcr-codebase/programming-elements/ArithmeticOperator.cs
@@ -0,0 +1,31 @@
+// Arithmetic operators 
+
+using System;
+class ArithmeticOperators{
+			static void Main(){
+			
+			int a=10;
+			int b=5;
+			
+			// addition
+			int add=a+b;
+			
+			// subtraction
+			int sub=a-b;
+			
+			// Multiplication
+			int multiple=a*b;
+			
+			// Division
+			int div=a/b;
+			
+			// modulus
+			int mod=a%b;
+			
+			Console.WriteLine(add);
+			Console.WriteLine(sub);
+			Console.WriteLine(multiple);
+			Console.WriteLine(div);
+			Console.WriteLine(mod);
+			}
+}
