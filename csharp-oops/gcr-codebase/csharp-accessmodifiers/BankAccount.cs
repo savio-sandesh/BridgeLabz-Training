7506b5ee6457bd9ecd1c25489464f08ff849c107
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-04 19:36:00 +0530

    04-01-2026

---
+++ csharp-oops/gcr-codebase/csharp-accessmodifiers/BankAccount.cs
@@ -0,0 +1,102 @@
+﻿namespace BridgeLabzTraining.csharp_accessmodifiers
+{
+	// bank account class
+	// demonstrates access modifiers using properties
+	internal class BankAccount
+	{
+		// public property
+		public long AccountNumber { get; set; }
+
+		// protected property
+		protected string AccountHolder { get; set; }
+
+		// private set property
+		public double Balance { get; private set; }
+
+		// stores all accounts
+		private static List<BankAccount> accountList = new List<BankAccount>();
+
+		// parameterized constructor
+		public BankAccount(long accountNumber, string accountHolder, double balance)
+		{
+			AccountNumber = accountNumber;
+			AccountHolder = accountHolder;
+			Balance = balance;
+			accountList.Add(this);
+		}
+
+		// public method to update balance
+		public void UpdateBalance(double newBalance)
+		{
+			Balance = newBalance;
+		}
+
+		// displays account details
+		public virtual void DisplayAccountDetails()
+		{
+			Console.WriteLine("Account Number : " + AccountNumber);
+			Console.WriteLine("Account Holder : " + AccountHolder);
+			Console.WriteLine("Balance        : INR " + Balance);
+		}
+
+		// displays all accounts
+		public static void DisplayAllAccounts()
+		{
+			foreach (BankAccount acc in accountList)
+			{
+				acc.DisplayAccountDetails();
+				Console.WriteLine();
+			}
+		}
+	}
+
+	// subclass to demonstrate protected access
+	internal class SavingsAccount : BankAccount
+	{
+		public SavingsAccount(long accountNumber, string accountHolder, double balance)
+			: base(accountNumber, accountHolder, balance)
+		{
+		}
+
+		// accessing public and protected members
+		public override void DisplayAccountDetails()
+		{
+			Console.WriteLine("Account Number : " + AccountNumber);   // public
+			Console.WriteLine("Account Holder : " + AccountHolder);   // protected
+			Console.WriteLine("Balance        : INR " + Balance);     // public get
+		}
+	}
+
+	internal class BankAccountManagement
+	{
+		private static void Main()
+		{
+			string choice;
+
+			do
+			{
+				Console.Write("Enter Account Number : ");
+				long accNo = Convert.ToInt64(Console.ReadLine());
+
+				Console.Write("Enter Account Holder Name : ");
+				string holder = Console.ReadLine();
+
+				Console.Write("Enter Balance : ");
+				double balance = Convert.ToDouble(Console.ReadLine());
+
+				new SavingsAccount(accNo, holder, balance);
+
+				Console.Write("Would you like to add more accounts? (yes/no) : ");
+				choice = Console.ReadLine().ToLower();
+
+				Console.WriteLine();
+			} while (choice == "yes");
+
+			// list all accounts
+			BankAccount.DisplayAllAccounts();
+
+			Console.WriteLine("Press Enter to exit...");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ csharp-oops/gcr-codebase/csharp-accessmodifiers/UniversityManagementSystem.cs
@@ -0,0 +1,85 @@
+﻿namespace BridgeLabzTraining.csharp_instance_classmembers
+{
+	// student class
+	// demonstrates use of public, protected and private members
+	internal class Student
+	{
+		// public member
+		public long rollNumber;
+
+		// protected member
+		protected string name;
+
+		// private member
+		private double cgpa;
+
+		// stores all students
+		private static List<Student> studentList = new List<Student>();
+
+		// parameterized constructor
+		public Student(long rollNumber, string name, double cgpa)
+		{
+			this.rollNumber = rollNumber;
+			this.name = name;
+			this.cgpa = cgpa;
+			studentList.Add(this);
+		}
+
+		// public method to access cgpa
+		public double GetCGPA()
+		{
+			return cgpa;
+		}
+
+		// displays student details
+		public void DisplayStudentDetails()
+		{
+			Console.WriteLine("Student Name : " + name);
+			Console.WriteLine("Roll Number  : " + rollNumber);
+			Console.WriteLine("CGPA       : " + cgpa);
+		}
+
+		// displays all students
+		public static void DisplayAllStudents()
+		{
+			foreach (Student s in studentList)
+			{
+				s.DisplayStudentDetails();
+				Console.WriteLine();
+			}
+		}
+	}
+
+	internal class UniversityManagementSystem
+	{
+		private static void Main()
+		{
+			string choice;
+
+			do
+			{
+				Console.Write("Enter Roll Number : ");
+				long roll = Convert.ToInt64(Console.ReadLine());
+
+				Console.Write("Enter Student Name : ");
+				string name = Console.ReadLine();
+
+				Console.Write("Enter CGPA: ");
+				double cgpa = Convert.ToDouble(Console.ReadLine());
+
+				new Student(roll, name, cgpa);
+
+				Console.Write("Would you like to add more? (yes/no) : ");
+				choice = Console.ReadLine().ToLower();
+
+				Console.WriteLine();
+			} while (choice == "yes");
+
+			// list all students
+			Student.DisplayAllStudents();
+
+			Console.WriteLine("Press enter to exit...");
+			Console.ReadLine();
+		}
+	}
+}
