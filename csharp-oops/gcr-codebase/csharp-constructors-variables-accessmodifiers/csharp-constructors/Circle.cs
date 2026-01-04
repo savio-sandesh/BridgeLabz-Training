1ef140333afb112adb7950193a71f7849c7cd2c4
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-04 21:22:13 +0530

    04-01-2026

---
+++ csharp-oops/gcr-codebase/csharp-constructors-variables-accessmodifiers/csharp-constructors/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_constructor
+{
+	internal class Circle
+	{
+		// Circle Class
+		// Demonstrates constructor chaining
+		// Default and user-defined radius initialization
+
+		private double radius;
+
+		// Default constructor
+		// Sets the default radius value to 1.0
+		public Circle() : this(1.0)
+		{
+		}
+
+		// Parameterized constructor
+		public Circle(double radius)
+		{
+			this.radius = radius;
+		}
+
+		// Displays circle details
+		public void DisplayDetails()
+		{
+			Console.WriteLine("Radius of circle: " + radius);
+			Console.WriteLine("Area of circle: " + (3.14 * radius * radius));
+		}
+
+		private static void Main(string[] args)
+		{
+			Circle c1 = new Circle();
+			c1.DisplayDetails();
+
+			Console.WriteLine();
+
+			Console.WriteLine("Enter radius for second circle:");
+			double value = double.Parse(Console.ReadLine()!);
+
+			Circle c2 = new Circle(value);
+			c2.DisplayDetails();
+		}
+	}
+}
---
+++ csharp-oops/gcr-codebase/csharp-constructors-variables-accessmodifiers/csharp-constructors/Person.cs
@@ -0,0 +1,49 @@
+﻿namespace BridgeLabzTraining.csharp_constructor
+{
+	// Person class
+	// Shows how a copy constructor creates a duplicate object
+	internal class Person
+	{
+		// Instance variables (encapsulated)
+		private string personName;
+
+		private int personAge;
+
+		// Constructor to initialize a new Person object
+		public Person(string personName, int personAge)
+		{
+			this.personName = personName;
+			this.personAge = personAge;
+		}
+
+		// Copy constructor
+		// Copies data from an existing Person object
+		public Person(Person existingPerson)
+		{
+			this.personName = existingPerson.personName;
+			this.personAge = existingPerson.personAge;
+		}
+
+		// Method to print person details
+		public void PrintDetails()
+		{
+			Console.WriteLine("Person Name : " + personName);
+			Console.WriteLine("Person Age  : " + personAge);
+		}
+
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Original person details:");
+			Person firstPerson = new Person("Sandesh", 23);
+			firstPerson.PrintDetails();
+
+			Console.WriteLine();
+
+			Console.WriteLine("Copied person details:");
+			Person secondPerson = new Person(firstPerson);
+			secondPerson.PrintDetails();
+
+			Console.ReadLine();
+		}
+	}
+}
