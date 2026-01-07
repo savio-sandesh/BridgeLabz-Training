fd6bd25e4e6f9f98573afcd622ffe990d4247c60
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-07 20:36:49 +0530

    UC2: Triggered daily wage calculation from EmployeeMain

---
+++ csharp-oops/scenario-based/EmployeeWage/EmployeeMain.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWage
+{
+	internal class EmployeeMain
+	{
+		private IEmployee employeeUtility;
+
+		public void Start()
+		{
+			// Implementation for starting employee wage computation
+			// You can add logic here as needed
+		}
+	}
+}
