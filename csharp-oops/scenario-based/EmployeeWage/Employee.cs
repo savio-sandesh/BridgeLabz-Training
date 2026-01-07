6c6e2d6ceb84023c1ef52387cae3cdb111f90867
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-07 16:32:53 +0530

    Added Employee model class

---
+++ csharp-oops/scenario-based/EmployeeWage/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeWage
+{
+	internal class Employee
+	{
+		public int EmployeeId { get; set; }
+
+		public string Name { get; set; }
+
+		public override string ToString()
+		{
+			return "Employee Id: " + this.EmployeeId +
+				   " Employee Name: " + this.Name;
+		}
+	}
+}
