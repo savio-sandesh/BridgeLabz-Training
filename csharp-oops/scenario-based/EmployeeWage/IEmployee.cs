f21a458d35cb20c56f77b0c3eec93ab7257b52bf
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-07 21:28:16 +0530

    UC5: Added monthly wage calculation method

--- csharp-oops/scenario-based/EmployeeWage/IEmployee.cs
+++ csharp-oops/scenario-based/EmployeeWage/IEmployee.cs
@@ -10,5 +10,9 @@
 
 		// UC-3: Calculate wage based on employee type (full-time / part-time)
 		int CalculateEmployeeWageByType();
+
+		// UC-5: Calculate monthly wage for 20 working days
+
+		int CalculateMonthlyWage();
 	}
 }
