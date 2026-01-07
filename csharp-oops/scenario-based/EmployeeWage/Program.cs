3ca29bd505d68c9ecb25edecaf833d467f9de4dc
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-07 20:38:50 +0530

    Refactored Program.cs to class-based entry point

--- csharp-oops/scenario-based/EmployeeWage/Program.cs
+++ csharp-oops/scenario-based/EmployeeWage/Program.cs
@@ -1,2 +1,13 @@
-﻿//// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Employee Wage Computation Program");
+﻿namespace EmployeeWage
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+			EmployeeMain employeeMain = new EmployeeMain();
+			employeeMain.Start();
+		}
+	}
+}
