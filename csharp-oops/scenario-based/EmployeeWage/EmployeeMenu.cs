b767afcd6a705c1d1b5bfabff724a62ba88be5d8
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-07 21:42:41 +0530

    UC6: Added wage calculation with limits option to menu

--- csharp-oops/scenario-based/EmployeeWage/EmployeeMenu.cs
+++ csharp-oops/scenario-based/EmployeeWage/EmployeeMenu.cs
@@ -19,6 +19,8 @@
 				Console.WriteLine("1. UC1 - Check Attendance");
 				Console.WriteLine("2. UC2 - Calculate Daily Wage");
 				Console.WriteLine("3. UC3 - Part Time Employee Wage");
+				Console.WriteLine("4. UC5 - Calculate Monthly Wage (20 Days)");
+				Console.WriteLine("5. UC6 - Wage with Max Hour & Day Limit");
 				Console.WriteLine("0. Exit");
 				Console.Write("Enter Choice: ");
 
@@ -27,17 +29,32 @@
 				switch (choice)
 				{
 					case 1:
+						// UC-1
 						employeeService.CheckAttendance();
 						break;
 
 					case 2:
+						// UC-2
 						int dailyWage = employeeService.CalculateDailyWage();
 						Console.WriteLine("Daily Wage: " + dailyWage);
 						break;
 
 					case 3:
+						// UC-3
 						int partTimeWage = employeeService.CalculateEmployeeWageByType();
 						Console.WriteLine("Employee Wage: " + partTimeWage);
+						break;
+
+					case 4:
+						// UC-5
+						int monthlyWage = employeeService.CalculateMonthlyWage();
+						Console.WriteLine("Monthly Wage (20 Days): " + monthlyWage);
+						break;
+
+					case 5:
+						// UC-6
+						int limitedWage = employeeService.CalculateWageWithHourAndDayLimit();
+						Console.WriteLine("Wage with Hour & Day Limit: " + limitedWage);
 						break;
 
 					case 0:
