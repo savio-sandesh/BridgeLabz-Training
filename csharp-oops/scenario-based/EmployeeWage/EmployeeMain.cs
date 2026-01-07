30fb4cc7787e2f441ed49656fdd2e8fa4f87bcb8
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-07 21:04:17 +0530

    UC4: Integrated employee menu into application flow

--- csharp-oops/scenario-based/EmployeeWage/EmployeeMain.cs
+++ csharp-oops/scenario-based/EmployeeWage/EmployeeMain.cs
@@ -2,21 +2,10 @@
 {
 	internal class EmployeeMain
 	{
-		private readonly IEmployee employeeUtility;
-
-		public EmployeeMain()
-		{
-			// Create object using interface reference
-			employeeUtility = new EmployeeUtilityImplementation();
-		}
-
 		public void Start()
 		{
-			// UC-2: Get daily wage
-			int dailyWage = employeeUtility.CalculateDailyWage();
-
-			// Display daily wage
-			Console.WriteLine("Daily Employee Wage: " + dailyWage);
+			EmployeeMenu menu = new EmployeeMenu();
+			menu.ShowChoices();
 		}
 	}
 }
