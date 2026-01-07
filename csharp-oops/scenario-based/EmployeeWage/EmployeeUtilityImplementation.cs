276e92c59a0b21400596cd352693fe6fb9ccecc7
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-07 16:40:23 +0530

    UC1: Implemented employee attendance check

---
+++ csharp-oops/scenario-based/EmployeeWage/EmployeeUtilityImplementation.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeWage
+{
+	internal class EmployeeUtilityImplementation : IEmployee
+	{
+		private static readonly Random _random = new Random();
+
+		// UC1 - Check Employee is Present or Absent
+		public void CheckAttendance()
+		{
+			int attendance = _random.Next(0, 2);
+
+			if (attendance == 1)
+				Console.WriteLine("Employee is Present");
+			else
+				Console.WriteLine("Employee is Absent");
+		}
+	}
+}
