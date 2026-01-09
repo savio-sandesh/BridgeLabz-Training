a9486dcb7a0a6d1c62993ee3a71ba1c6de96b94c
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-09 12:09:37 +0530

    Define workout tracking interface using abstraction

---
+++ csharp-oops/scenario-based/FitTrack/ITrackable.cs
@@ -0,0 +1,12 @@
+﻿namespace FitTrack
+{
+	// interface defining workout tracking behavior
+	internal interface ITrackable
+	{
+		void TrackWorkout();
+
+		int CalculateCaloriesBurned();
+
+		void DisplaySummary();
+	}
+}
