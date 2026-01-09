a2f91151d3823bc07735e50833bedfb67fed7054
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-09 12:11:24 +0530

    Add strength workout implementation using sets and reps

---
+++ csharp-oops/scenario-based/FitTrack/StrengthWorkout.cs
@@ -0,0 +1,26 @@
+﻿namespace FitTrack
+{
+	// derived class representing strength-based workouts
+	internal class StrengthWorkout : Workout
+	{
+		public int Sets { get; set; }
+		public int Reps { get; set; }
+
+		public override void TrackWorkout()
+		{
+			CaloriesBurned = CalculateCaloriesBurned();
+		}
+
+		public override int CalculateCaloriesBurned()
+		{
+			return Sets * Reps * 2;
+		}
+
+		public override void DisplaySummary()
+		{
+			base.DisplaySummary();
+			Console.WriteLine($"Sets: {Sets}, Reps: {Reps}");
+			Console.WriteLine("Workout Type: Strength");
+		}
+	}
+}
