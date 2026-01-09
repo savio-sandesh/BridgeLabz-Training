733b782119c702479ec2608cf402c15c9eedc279
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-09 12:10:52 +0530

    Add cardio workout implementation with calorie calculation

---
+++ csharp-oops/scenario-based/FitTrack/CardioWorkout.cs
@@ -0,0 +1,25 @@
+﻿namespace FitTrack
+{
+	// derived class representing cardio-based workouts
+	internal class CardioWorkout : Workout
+	{
+		public double DistanceInKm { get; set; }
+
+		public override void TrackWorkout()
+		{
+			CaloriesBurned = CalculateCaloriesBurned();
+		}
+
+		public override int CalculateCaloriesBurned()
+		{
+			return (int)(DistanceInKm * 60);
+		}
+
+		public override void DisplaySummary()
+		{
+			base.DisplaySummary();
+			Console.WriteLine($"Distance: {DistanceInKm} km");
+			Console.WriteLine("Workout Type: Cardio");
+		}
+	}
+}
