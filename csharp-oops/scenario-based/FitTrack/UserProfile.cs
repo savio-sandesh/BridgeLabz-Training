287a63171cc79754ef9644c10684d51d25a781e3
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-09 12:09:12 +0530

    Add user profile model with workout history support

---
+++ csharp-oops/scenario-based/FitTrack/UserProfile.cs
@@ -0,0 +1,25 @@
+﻿namespace FitTrack
+{
+	internal class UserProfile
+	{
+		public int UserId { get; set; }
+		public string Name { get; set; }
+
+		private List<Workout> workouts = new List<Workout>();
+
+		public void AddWorkout(Workout workout)
+		{
+			workouts.Add(workout);
+		}
+
+		public void ShowWorkoutHistory()
+		{
+			foreach (var workout in workouts)
+			{
+				Console.WriteLine();
+				workout.DisplaySummary();
+				Console.WriteLine();
+			}
+		}
+	}
+}
