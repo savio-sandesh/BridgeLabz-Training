f51777285aa85826eed0620d934dbbd1ea6afeab
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-12 11:53:55 +0530

    Reverse a String Using StringBuilder

---
+++ csharp-dsa/gcr-codebase/csharp-searching-stringbuilder-streamreader/ReverseString.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace BridgeLabzTraining.csharp_searching
+{
+	internal class ReverseString
+	{
+		private static void Main(string[] args)
+		{
+			Console.Write("Enter a string: ");
+			string input = Console.ReadLine();
+
+			// StringBuilder with predefined capacity (best practice)
+			StringBuilder reversed = new StringBuilder(input.Length);
+
+			// Reverse logic
+			for (int i = input.Length - 1; i >= 0; i--)
+			{
+				reversed.Append(input[i]);
+			}
+
+			Console.WriteLine("Reversed String: " + reversed.ToString());
+		}
+	}
+}
