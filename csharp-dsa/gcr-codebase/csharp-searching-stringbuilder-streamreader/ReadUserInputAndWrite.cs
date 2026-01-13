8cb2d9a89cef811039e6e1f74b3024bdb72632a0
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-13 19:04:06 +0530

    Read User Input and Write to File Using StreamReader

---
+++ csharp-dsa/gcr-codebase/csharp-searching-stringbuilder-streamreader/ReadUserInputAndWrite.cs
@@ -0,0 +1,21 @@
+﻿namespace BridgeLabzTraining.csharp_searching_stringbuilder_streamreader
+{
+	internal class ReadUserInputAndWrite
+	{
+		private static void Main(string[] args)
+		{
+			Console.Write("Enter the file path where data should be written: ");
+			string filePath = Console.ReadLine().Trim('"');
+
+			Console.Write("Enter text to write into the file: ");
+			string userInput = Console.ReadLine();
+
+			using (StreamWriter writer = new StreamWriter(filePath))
+			{
+				writer.WriteLine(userInput);
+			}
+
+			Console.WriteLine("\nData written to file successfully.");
+		}
+	}
+}
