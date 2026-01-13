64e0b5e6e8bc7870076e6d5dda1b92ed0a8510b0
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-13 19:07:11 +0530

    Search for the First Negative Number

---
+++ csharp-dsa/gcr-codebase/csharp-searching-stringbuilder-streamreader/FirstNegativeNumber.cs
@@ -0,0 +1,38 @@
+﻿namespace BridgeLabzTraining.csharp_searching_stringbuilder_streamreader
+{
+	internal class FirstNegativeNumber
+	{
+		private static void Main(string[] args)
+		{
+			Console.Write("Enter number of elements: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			int[] arr = new int[n];
+
+			// Taking array input
+			for (int i = 0; i < n; i++)
+			{
+				Console.Write($"Enter element {i + 1}: ");
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			bool found = false;
+
+			// Linear Search for first negative number
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (arr[i] < 0)
+				{
+					Console.WriteLine("\nFirst negative number is: " + arr[i]);
+					found = true;
+					break;
+				}
+			}
+
+			if (!found)
+			{
+				Console.WriteLine("\nNo negative number found in the array.");
+			}
+		}
+	}
+}
