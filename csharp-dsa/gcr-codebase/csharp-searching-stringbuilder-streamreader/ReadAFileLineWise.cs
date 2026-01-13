7bbda1d14e9481576791883febd6983614367f36
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-13 18:57:36 +0530

    Read a File Line by Line Using StreamReader

---
+++ csharp-dsa/gcr-codebase/csharp-searching-stringbuilder-streamreader/ReadAFileLineWise.cs
@@ -0,0 +1,33 @@
+﻿//namespace BridgeLabzTraining.csharp_searching_stringbuilder_streamreader
+//{
+//	internal class ReadAFileLineWise
+//	{
+//		private static void Main(string[] args)
+//		{
+//			Console.Write("Enter the file path: ");
+//			string filePath = Console.ReadLine();
+
+//			try
+//			{
+//				using (StreamReader reader = new StreamReader(filePath))
+//				{
+//					string line;
+//					Console.WriteLine("\n--- File Content ---");
+
+//					while ((line = reader.ReadLine()) != null)
+//					{
+//						Console.WriteLine(line);
+//					}
+//				}
+//			}
+//			catch (FileNotFoundException)
+//			{
+//				Console.WriteLine("Error: File not found.");
+//			}
+//			catch (IOException ex)
+//			{
+//				Console.WriteLine("I/O Error: " + ex.Message);
+//			}
+//		}
+//	}
+//}
