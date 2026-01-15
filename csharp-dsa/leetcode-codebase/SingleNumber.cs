2c0588e7bb294578284932b0f310a41d2ac8108c
savio-sandesh <sandesh.cse00@gmail.com>
2026-01-15 11:05:53 +0530

    Solve single number problem using XOR with constant space

---
+++ csharp-dsa/leetcode-codebase/SingleNumber.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int res = 0;
+        for(int i =0;i < nums.Length;i++){
+            res= res^nums[i];
+        }
+        return res;
+
+    }
+}
+
+
