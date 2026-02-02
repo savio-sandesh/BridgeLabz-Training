66136e506343bb9a40ec0325508490be6ea376e1
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2026-02-02 21:36:19 +0530

    Read a JSON file and print all keys and values.

---
+++ csharp-io-programming/gcr-codebase/csharp-json-datahandling/JsonKeysValuesApp/JsonKeysValuesApp.cs
@@ -0,0 +1,75 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+// Reads a JSON file and prints all keys and values recursively
+public class JsonKeysValuesApp
+{
+    public static void Main(string[] args)
+    {
+        // 11. Read a JSON file and print all keys and values
+
+        Console.WriteLine("Read JSON & Print All Keys and Values\n");
+
+        string jsonText = @"{
+            ""name"": ""Rahul Kumar"",
+            ""age"": 24,
+            ""city"": ""Mathura"",
+            ""isStudent"": true,
+            ""marks"": [85, 92, 78],
+            ""address"": {
+                ""street"": ""Civil Lines"",
+                ""pincode"": 281001
+            }
+        }";
+
+        string filePath = "sample.json";
+        File.WriteAllText(filePath, jsonText);
+
+        Console.WriteLine("Dummy JSON file created: " + filePath);
+
+        try
+        {
+            string content = File.ReadAllText(filePath);
+            JToken token = JToken.Parse(content);
+
+            Console.WriteLine("\nAll keys and values:");
+            PrintJson(token, "");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error reading or parsing JSON: " + ex.Message);
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+
+    // Recursively prints JSON keys and values
+    private static void PrintJson(JToken token, string indent)
+    {
+        if (token is JObject obj)
+        {
+            foreach (JProperty prop in obj.Properties())
+            {
+                Console.WriteLine($"{indent}{prop.Name}:");
+                PrintJson(prop.Value, indent + "  ");
+            }
+        }
+        else if (token is JArray array)
+        {
+            Console.WriteLine(indent + "[");
+            foreach (JToken item in array)
+            {
+                PrintJson(item, indent + "  ");
+            }
+            Console.WriteLine(indent + "]");
+        }
+        else
+        {
+            Console.WriteLine(indent + token);
+        }
+    }
+}
