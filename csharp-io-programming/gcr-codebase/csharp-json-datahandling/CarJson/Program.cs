549dd2b019226588f4a67638c4f3674ed7ec0bef
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2026-02-02 16:49:07 +0530

    Convert a C# object (Car class) into JSON format.

---
+++ csharp-io-programming/gcr-codebase/csharp-json-datahandling/CarJson/Program.cs
@@ -0,0 +1,42 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+using Newtonsoft.Json;
+
+// Demonstrates converting a C# Car object into JSON format
+public class CarJsonApp
+{
+    // Model class representing a car
+    public class Car
+    {
+        public string brand;
+        public string model;
+        public int year;
+        public double price;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Convert a C# object (Car class) into JSON format
+
+        Console.WriteLine("C# Object to JSON Conversion (Car)\n");
+
+        Car myCar = new Car
+        {
+            brand = "Toyota",
+            model = "Fortuner",
+            year = 2022,
+            price = 4500000.50
+        };
+
+        string json = JsonConvert.SerializeObject(myCar, Formatting.Indented);
+
+        Console.WriteLine("Car JSON:");
+        Console.WriteLine(json);
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
