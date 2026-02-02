1a2fb27a60066aa0586948ac89e3989b8d989c6c
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2026-02-02 16:56:11 +0530

    Merge two JSON objects into one.

---
+++ csharp-io-programming/gcr-codebase/csharp-json-datahandling/JsonMerge/Program.cs
@@ -0,0 +1,50 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Husing System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class JsonMerge
+{
+    public static void Main(string[] args)
+    {
+        // Merge two JSON objects into one
+
+        Console.WriteLine("Merge Two JSON Objects\n");
+
+        string json1 = @"{
+            ""name"": ""Aman"",
+            ""age"": 22,
+            ""city"": ""Mathura""
+        }";
+
+        string json2 = @"{
+            ""marks"": 85,
+            ""grade"": ""A"",
+            ""school"": ""KV Mathura""
+        }";
+
+        try
+        {
+            JObject obj1 = JObject.Parse(json1);
+            JObject obj2 = JObject.Parse(json2);
+
+            // Merge json2 into json1
+            obj1.Merge(obj2, new JsonMergeSettings
+            {
+                MergeArrayHandling = MergeArrayHandling.Merge
+            });
+
+            string mergedJson = obj1.ToString(Formatting.Indented);
+
+            Console.WriteLine("Merged JSON:");
+            Console.WriteLine(mergedJson);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error merging JSON: " + ex.Message);
+        }
+
+        Console.WriteLine("\nPress any key...");
+        Console.ReadKey();
+    }
+}
