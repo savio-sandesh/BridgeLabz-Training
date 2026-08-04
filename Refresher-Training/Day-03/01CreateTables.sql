70f1a715920ff65656d8fa727b56be464226f0e6
savio-sandesh <sandesh.cse00@gmail.com>
2026-08-04 21:09:08 +0530

    Create health clinic database schema with core tables

---
+++ Refresher-Training/Day-03/01CreateTables.sql
@@ -0,0 +1,94 @@
+USE Health_Clinic;
+GO
+
+
+-- PATIENT TABLE
+
+CREATE TABLE Patient
+(
+    PatientId INT IDENTITY(1,1) PRIMARY KEY,
+
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    Gender VARCHAR(20),
+
+    Phone VARCHAR(15) NOT NULL,
+    Email VARCHAR(100),
+    Address VARCHAR(255)
+
+    CONSTRAINT CHK_Patient_Gender
+    CHECK (Gender IN ('Male', 'Female', 'Other'))
+);
+
+GO
+
+
+-- DOCTOR TABLE
+
+CREATE TABLE Doctor
+(
+    DoctorId INT IDENTITY(1,1) PRIMARY KEY,
+
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Specialization VARCHAR(100) NOT NULL,
+
+    Phone VARCHAR(15),
+    Email VARCHAR(100)
+);
+
+GO
+
+
+-- APPOINTMENT TABLE
+
+CREATE TABLE Appointment
+(
+    AppointmentId INT IDENTITY(1,1) PRIMARY KEY,
+
+    PatientId INT NOT NULL,
+    DoctorId INT NOT NULL,
+
+    AppointmentDate DATE NOT NULL,
+    AppointmentTime TIME NOT NULL,
+
+    Reason VARCHAR(255),
+    Status VARCHAR(20) NOT NULL DEFAULT 'Scheduled',
+
+    CONSTRAINT FK_Appointment_Patient
+        FOREIGN KEY (PatientId)
+        REFERENCES Patient(PatientId),
+
+    CONSTRAINT FK_Appointment_Doctor
+        FOREIGN KEY (DoctorId)
+        REFERENCES Doctor(DoctorId),
+
+    CONSTRAINT CHK_Appointment_Status
+        CHECK (Status IN ('Scheduled', 'Completed', 'Cancelled'))
+);
+
+GO
+
+CREATE TABLE Room
+(
+    RoomId INT IDENTITY(1,1) PRIMARY KEY,
+    RoomNumber VARCHAR(20) UNIQUE,
+    FloorNo INT,
+    RoomType VARCHAR(50)
+);
+
+-- changes in doctor table
+
+ALTER TABLE Doctor
+ADD RoomId INT;
+
+ALTER TABLE Doctor
+ADD CONSTRAINT FK_Doctor_Room
+FOREIGN KEY(RoomId)
+REFERENCES Room(RoomId);
+
+SELECT * From Patient;
+SELECT * From Doctor;
+SELECT * From Appointment;
+SELECT * From Room;
